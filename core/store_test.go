36eeaf2eaa0203489ef6f59f20acb150c8af8c64
James Ravn <james.ravn@sky.uk>
2018-01-05 10:11:30 +0000

    Add tests for multiple URLs

--- core/store_test.go
+++ core/store_test.go
@@ -5,15 +5,57 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/docker/libkv"
-	"github.com/docker/libkv/store/mock"
+	libkvmock "github.com/docker/libkv/store/mock"
+	"github.com/docker/libkv/store"
 )
 
-func TestMultipleURIs(t *testing.T) {
+type storeMock struct {
+	libkvmock.Mock
+}
+
+func (s *storeMock) mockNew() func(endpoints []string, options *store.Config) (store.Store, error) {
+	return func(endpoints []string, options *store.Config) (store.Store, error) {
+		s.Endpoints = endpoints
+		s.Options = options
+		return &s.Mock, nil
+	}
+}
+
+func TestMultipleURLs(t *testing.T) {
 	assert := assert.New(t)
-	libkv.AddStore("mock", mock.New)
+	m := storeMock{}
+	libkv.AddStore("mock", m.mockNew())
+	m.On("List", "/").Return([]*store.KVPair{}, nil)
 
 	storeURLs := []string{"mock://127.0.0.1:2000", "mock://127.0.0.2:2001", "mock://127.0.0.3:2002"}
 	store, err := NewStore(storeURLs, "/", "/", 60, &Context{})
+
 	assert.NoError(err)
+	assert.Equal([]string{"127.0.0.1:2000", "127.0.0.2:2001", "127.0.0.3:2002"}, m.Endpoints)
+
 	store.Close()
+}
+
+func TestErrorIfSchemeMismatch(t *testing.T) {
+	assert := assert.New(t)
+	m := storeMock{}
+	libkv.AddStore("mock", m.mockNew())
+	m.On("List", "/").Return([]*store.KVPair{}, nil)
+
+	storeURLs := []string{"mock://127.0.0.1:2000", "mismatch://127.0.0.2:2001", "mock://127.0.0.3:2002"}
+	_, err := NewStore(storeURLs, "/", "/", 60, &Context{})
+
+	assert.Error(err)
+}
+
+func TestErrorIfPathMismatch(t *testing.T) {
+	assert := assert.New(t)
+	m := storeMock{}
+	libkv.AddStore("mock", m.mockNew())
+	m.On("List", "/").Return([]*store.KVPair{}, nil)
+
+	storeURLs := []string{"mock://127.0.0.1:2000", "mock://127.0.0.2:2001/mismatched/path/", "mock://127.0.0.3:2002"}
+	_, err := NewStore(storeURLs, "/", "/", 60, &Context{})
+
+	assert.Error(err)
 }
